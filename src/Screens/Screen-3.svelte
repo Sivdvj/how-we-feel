e9c24585ceeda4f1a6b7ba67c8dcba79d3ddd3bb
Sivdvj <sivaranjani.dvj@gmail.com>
2025-12-15 16:34:52 +0530

    fix: emotion screen panning

--- src/Screens/Screen-3.svelte
+++ src/Screens/Screen-3.svelte
@@ -7,18 +7,28 @@
     let offsetX = 0, offsetY = 0, timeout
     export let goto
     onMount(() =>{
+        let prevX = 0, prevY = 0
+        window.addEventListener('touchstart', (e) => {
+            prevX = e.touches[0].pageX;
+            prevY = e.touches[0].pageY;
+        })
         window.addEventListener('touchmove', (e) => {
-            let x = e.touches[0].pageX - window.innerWidth/2;
-            let y = e.touches[0].pageY - window.innerHeight/2; //mouse position relative to the center - now its like a 2D plane
-
-            offsetX = x * 1.5;
-            offsetY = y * 1.5;
-            clearTimeout(timeout);
+            let x = e.touches[0].pageX;
+            let y = e.touches[0].pageY;
             
-            timeout = setTimeout(() => {            
-                offsetX += 50;
-                offsetY += 50;
-            }, 100);
+            let deltaX = x - prevX;
+            let deltaY = y - prevY;
+            
+            offsetX += deltaX;
+            offsetY += deltaY;
+            
+            prevX = x;
+            prevY = y;
+
+            if(offsetX > 600) offsetX = 600;
+            if(offsetX < -600) offsetX = -600;
+            if(offsetY > 600) offsetY = 600;
+            if(offsetY < -600) offsetY = -600;     
         })
     })
 
@@ -35,7 +45,7 @@
                 <Icons icon = 'material-symbols-light:search-rounded' opacity = {80} width = {8} height = {8} />
             </div>
         </div>
-        <div class = 'absolute transition-transform left-1/2 top-1/2 duration-1000 ease-out' style = 'transform: translate(calc(-50% + {offsetX}px), calc(-50% + {offsetY}px))'>
+        <div class = 'absolute left-1/2 top-1/2' style = 'transform: translate(calc(-50% + {offsetX}px), calc(-50% + {offsetY}px))'>
             <Emotion {offsetX} {offsetY} />
         </div>
         <div class = 'w-[450px] h-[100px] bg-gray-800 opacity-98 rounded-full absolute bottom-5 left-0 right-0 mx-auto flex flex-row justify-between items-center pb-5 pt-5 pl-10 pr-2'>
