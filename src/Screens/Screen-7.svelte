f7f63b3da6e79f5fa2a286b23772d8939dabea98
Sivdvj <151998553+Sivdvj@users.noreply.github.com>
2025-12-18 17:33:31 +0530

    chore: images mapped to colors

--- src/Screens/Screen-7.svelte
+++ src/Screens/Screen-7.svelte
@@ -1,13 +1,18 @@
 <script>
-    import Ecomp from '../lib/EmotionComp.svelte'
-    import Icons from "../lib/Icons.svelte"
-    export let goto  
+	import Ecomp from "../lib/EmotionComp.svelte";
+	import Icons from "../lib/Icons.svelte";
+	export let goto;
 </script>
 
-<div class = 'w-full h-full p-4 overflow-y-auto scrollbar-hide scroll-smooth space-y-5'>
-    <button class = 'text-3xl text-white flex justify-center w-full opacity-80' on:click={() => goto('screen1')}>
-        <Icons icon = 'material-symbols-light:family-home-outline-rounded' size={20} width={20} height={20} showCircle={false}/>
-    </button>
-    <Ecomp />
-    
+<div class="scrollbar-hide h-full w-full space-y-5 overflow-y-auto scroll-smooth p-4">
+	<button class="flex w-full justify-center text-3xl text-white opacity-80" on:click={() => goto("screen1")}>
+		<Icons icon="material-symbols-light:family-home-outline-rounded" size={20} width={20} height={20} showCircle={false} />
+	</button>
+	<Ecomp />
 </div>
+
+<style>
+	.scrollbar-hide::-webkit-scrollbar {
+		display: none;
+	}
+</style>
--- src/lib/EmotionComp.svelte
+++ src/lib/EmotionComp.svelte
@@ -1,27 +1,32 @@
 <script>
-    import {onMount} from 'svelte'
-    import Ecomp1 from './Ecomp1.svelte'
-    import { request } from './Auth';
-    
-    let data = {}
-    onMount(async () => {
-        data = await request("/data")
-        data = data.Emotion
-    })
-    
-    // TODO: change image paths based on color
-    function getImage(color){
-        if(color == "#ffa500") return "assets/1.png"
-        else if(color == "#83f28f") return "assets/2.png"
-        else if(color == "#90d5ff") return "assets/3.png"
-        else return "assets/4.png"
-    }
+	import { onMount } from "svelte";
+	import Ecomp1 from "./Ecomp1.svelte";
+	import { request } from "./Auth";
+
+	let data = {};
+	onMount(async () => {
+		data = await request("/data");
+		data = data.Emotion;
+	});
+
+	function getImage(color) {
+		const hex = color.replace("#", "");
+		const r = parseInt(hex.substring(0, 2), 16);
+		const g = parseInt(hex.substring(2, 4), 16);
+		const b = parseInt(hex.substring(4, 6), 16);
+
+		if (b > r && b > g * 0.8) return "/assets/3.png"; // Blue
+		if (g > r && g > b) return "/assets/2.png"; // Green
+		if (g > r * 0.75) return "/assets/4.png"; // Yellow
+		return "/assets/1.png"; // Red
+	}
 </script>
+
 <main>
-    <div class = 'flex flex-col-reverse'>
-        {console.log(data)}
-        {#each Object.entries(data) as [id, userdata]}
-            <Ecomp1 color={userdata.color} text={userdata.emotion} image={getImage(userdata.color)}/>
-        {/each}
-    </div>
+	<div class="flex flex-col-reverse">
+		{console.log(data)}
+		{#each Object.entries(data) as [id, userdata]}
+			<Ecomp1 color={userdata.color} text={userdata.emotion} image={getImage(userdata.color)} />
+		{/each}
+	</div>
 </main>
