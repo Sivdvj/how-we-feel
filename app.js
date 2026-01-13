274716fcf5c52de083cea4294b2349f6638a349d
Sivdvj <151998553+Sivdvj@users.noreply.github.com>
2026-01-13 13:24:53 +0530

    chore: shift config to env vars

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-	host: "localhost",
-	port: 5432,
-	user: "postgres",
-	password: "postgres",
-	database: "postgres",
+	connectionString: process.env.DATABASE_URL,
 });
 
 const app = express();
